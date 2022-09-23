1bab70c2b9b35de93c2de00ce7dbf2d5c0b8b9cc
Vladimir Vovk <57999167+VladimirVovk97@users.noreply.github.com>
2022-09-23 18:16:02 +0300

    edit options

--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "./Options.styled.jsx";
-import { Controls, MainHeading, Button } from "./Options.styled";
+import { Controls, Item, Button } from "./Options.styled";
+import PropTypes from "prop-types";
 
-const FeedbackOptions = ({
-  firstIncrement,
-  secondIncrement,
-  thirdIncrement,
-}) => (
-  <Controls>
-    <MainHeading>Please leave feedback</MainHeading>
-    <Button onClick={firstIncrement}>Good</Button>
-    <Button onClick={secondIncrement}>Neutral</Button>
-    <Button onClick={thirdIncrement}>Bad</Button>
-  </Controls>
-);
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <Controls>
+      {options.map((option) => (
+        <Item key={option}>
+          <Button
+            name={option}
+            type="button"
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </Button>
+        </Item>
+      ))}
+    </Controls>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
 export default FeedbackOptions;
--- src/Components/Options/Options.styled.jsx
+++ src/Components/Options/Options.styled.jsx
@@ -1,40 +1,12 @@
 import styled from "@emotion/styled";
 
-export const Controls = styled.div`
-  border: none;
-  padding: 5px 10px;
-  background-color: white;
-  color: black;
-  font-weight: 500;
-  margin-left: 5px;
-  margin-right: 5px;
-  font-family: inherit;
-  outline: none;
+export const Controls = styled.ul`
+  display: flex;
+  justify-content: center;
 
-  > button {
-    border: none;
-    padding: 5px 10px;
-    background-color: white;
-    color: black;
-    font-weight: 500;
-    margin-left: 5px;
-    margin-right: 5px;
-    font-family: inherit;
-    cursor: pointer;
-    outline: none;
-    box-shadow: 1px 2px 2px 0 rgb(0 0 0 / 0.5);
-    transition: box-shadow 250ms linear;
-    border-radius: 5px;
-    border: 1px solid gray;
-
-    &:hover,
-    focus,
-    active {
-      background-color: rgba(8, 75, 201, 0.295);
-      color: white;
-      box-shadow: 2px 5px 2px 0 rgb(0 0 0 / 0.5);
-    }
-  }
+  padding: 0;
+  margin: 0 0 25px;
+  list-style: none;
 `;
 
 export const MainHeading = styled.h1`
@@ -56,12 +28,19 @@ export const Button = styled.button`
   transition: box-shadow 250ms linear;
   border-radius: 5px;
   border: 1px solid gray;
+  text-transform: capitalize;
+  height: 35px;
 
   &:hover,
   focus,
   active {
-    background-color: rgba(8, 75, 201, 0.295);
-    color: white;
+    background-color: rgba(56, 55, 55, 0.219);
+    color: black;
     box-shadow: 2px 5px 2px 0 rgb(0 0 0 / 0.5);
   }
+`;
+
+export const Item = styled.li`
+not(:last-child) {
+  margin-right: 10px;
 `;
